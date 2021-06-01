be3303937c53ee28dfed3e743ceb70cc75be7de8
Luca Fruzza <lfruzza@hotmail.co.uk>
2021-06-01 20:15:14 +0100

    :sparkles: add PunchCafe.Stream.collect_to_map

--- implementations/stream.ex
+++ implementations/stream.ex
@@ -21,9 +21,18 @@ defmodule PunchCafe.Stream do
         if predicate.(value), do: value, else: :empty
     end
 
-    def execute_list(%PunchCafe.Stream{} = stream) do
+    def collect_to_list(%PunchCafe.Stream{} = stream) do
         map_and_drop_empty(stream.collection, stream.operation)
     end
+
+    def collect_to_map(%PunchCafe.Stream{} = stream, key_generator) do
+        insert_into_map(map_and_drop_empty(stream.collection, stream.operation), key_generator)
+    end
+
+    defp insert_into_map([head|tail], key_generator) do
+        Map.put_new(insert_into_map(tail, key_generator), key_generator.(head), head)
+    end
+    defp insert_into_map([], _key_generator), do: %{}
 
     defp map_and_drop_empty([], _function), do: [] 
     defp map_and_drop_empty([head|tail], function) do
@@ -40,6 +49,11 @@ result = PunchCafe.Stream.of([1, 2])
 |> PunchCafe.Stream.map(&(&1 + 1))
 |> PunchCafe.Stream.map(&(&1 * 3))
 |> PunchCafe.Stream.filter(&(rem(&1,2)==0))
-|> PunchCafe.Stream.execute_list()
+|> PunchCafe.Stream.collect_to_list()
+
+result2 = PunchCafe.Stream.of([1, 2])
+|> PunchCafe.Stream.map(&(&1 + 1))
+|> PunchCafe.Stream.collect_to_map(&Integer.to_string/1)
 
 IO.inspect result
+IO.inspect result2
