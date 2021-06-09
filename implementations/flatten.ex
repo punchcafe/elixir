e272d8d1fd3811c4138dd8aa6b5a84f83c9e8b8a
Luca Fruzza <lfruzza@hotmail.co.uk>
2021-06-09 20:47:57 +0100

    :recycle:

--- implementations/flatten.ex
+++ implementations/flatten.ex
@@ -6,8 +6,8 @@ defmodule PunchCafe.Enum do
 
     defp flatten_recur([], accum), do: accum
     defp flatten_recur([ head | tail ], accum) do
-        result = if is_list(head), do: flatten_recur(head, accum), else: [ head | accum ]
+        result = if is_list(head), do: flatten_recur(head, []), else: [head]
         flatten_recur(tail, accum) ++ result
     end
 end
-IO.inspect(PunchCafe.Enum.flatten([1,2,[3,[4,5]]]))
+IO.inspect(PunchCafe.Enum.flatten([1,2,[3,[4,5], 6, 7 ,[8, 9]]]))
