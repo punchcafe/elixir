b4f36cd431176ccd6e1b3723a8639e2dadb4eedd
Luca Fruzza <lfruzza@hotmail.co.uk>
2021-06-12 19:53:15 +0100

    :sparkles: final chapter 10 exercise

--- programming-elixir/chapter_10_lists_and_recursion.ex
+++ programming-elixir/chapter_10_lists_and_recursion.ex
@@ -20,4 +20,28 @@ defmodule Primes do
     end
 end
 
+defmodule Taxables do
+    def apply_tax([ id: _id, ship_to: ship_to, net_amount: net_amount ] = order_item, tax_rates ) do
+      rate = Keyword.get(tax_rates, ship_to, 0)
+      Keyword.put( order_item, :net_amount, net_amount + rate*net_amount)
+    end
+    def apply_taxes_to_order(orders, tax) do
+        Enum.map(orders, fn order -> apply_tax(order, tax) end)
+    end
+end
+
 IO.inspect(Primes.get_primes_from_two_to(10))
+
+tax_rates = [NC: 0.075, TX: 0.08]
+orders = [
+    [id: 123, ship_to: :NC, net_amount: 100.00],
+    [id: 124, ship_to: :OK, net_amount: 35.50],
+    [id: 125, ship_to: :TX, net_amount: 24.00],
+    [id: 126, ship_to: :TX, net_amount: 44.80],
+    [id: 127, ship_to: :NC, net_amount: 25.00],
+    [id: 128, ship_to: :MA, net_amount: 10.00],
+    [id: 129, ship_to: :CA, net_amount: 102.00],
+    [id: 130, ship_to: :NC, net_amount: 50.00]
+]
+
+IO.inspect(Taxables.apply_taxes_to_order(orders, tax_rates))
