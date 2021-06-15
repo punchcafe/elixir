7212c61b747ab037bd375285565ab4df89c0a6ee
Luca Fruzza <lfruzza@hotmail.co.uk>
2021-06-15 17:50:28 +0100

    :hankey: good enough ex solution

--- programming-elixir/chapter_11_ex.ex
+++ programming-elixir/chapter_11_ex.ex
@@ -67,6 +67,44 @@ defmodule StringsAndCharlists do
     end
 end
 
+defmodule OrderRecord do
+    defstruct id: nil, city: nil, net_amount: nil
+end
+
+defmodule TaxReader do
+
+    def parse_file(file_location) do
+        file_stream = File.stream!(file_location)
+        column_config = Enum.at(file_stream, 0)
+        |> extract_column_config()
+        Stream.map(file_stream, fn line -> parse_line(line, column_config) end)
+        |> Enum.slice(1..10_000)
+    end
+    defp parse_line(line, column_config) do
+        String.split(line, ",")
+        |> Enum.with_index()
+        |> Enum.reduce(%{}, fn elem, acc -> reduce_line_elements(elem, acc, column_config) end)
+        |> parse_map()
+    end
+    defp parse_map(%{id: id, ship_to: city, net_amount: net_amount}), do: %OrderRecord{id: id, city: String.to_atom(city), net_amount: Float.parse(net_amount) }
+    defp parse_map(_), do: nil
+
+    defp reduce_line_elements({value, index} = boi, acc, column_config) do
+        key = Map.get(column_config, index)
+        Map.put_new(acc, key, value)
+    end
+
+    defp extract_column_config(first_line_in_csv) do
+        String.split(first_line_in_csv, ",")
+        |> Enum.reduce(%{index: 0}, &reduce_columns_to_index/2)
+    end
+    defp reduce_columns_to_index(elem, acc) do
+        Map.put_new(acc, acc.index, String.to_atom(String.trim(elem)))
+        |> increment_index()
+    end
+    defp increment_index(map), do: %{ map | index: map.index + 1 }
+end
+
 IO.inspect(StringsAndCharlists.evaluate('1 + 2'))
 IO.inspect(StringsAndCharlists.evaluate('5 - 1'))
 IO.inspect(StringsAndCharlists.evaluate('1 * 2'))
@@ -74,3 +112,5 @@ IO.inspect(StringsAndCharlists.evaluate('5 / 5'))
 
 IO.inspect(StringsAndCharlists.center(["hello", "world", "how", "are", "u"]))
 IO.inspect(StringsAndCharlists.capitalize_sentences("anyway, this dog. yeah, what about it. well, I capitalized it."))
+
+IO.inspect(TaxReader.parse_file("ch_11_shipping.dat"))
