43ac4955cc796c504a69948c2100706cfd84e349
Luca Fruzza <lfruzza@hotmail.co.uk>
2021-06-25 15:52:49 +0100

    :tada: add chapter 13 project

---
+++ programming-elixir/chapter_13/lib/chapter13.ex
@@ -0,0 +1,18 @@
+defmodule Chapter13 do
+  @moduledoc """
+  Documentation for `Chapter13`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Chapter13.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
